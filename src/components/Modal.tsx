9c5c2b86cd4dabea036f96a3b42d8edcd26b9ee9
gregfenton <greg.fenton@gmail.com>
2020-09-05 20:18:36 -0400

    [docs] Snack example code fixes/enhancements
    - FIX - the "Show" button was clickable when the modal was open, stopping the modal from closing
    - ENHANCE - the style of the modal was black-on-dark-grey; now it has white background & some padding

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -69,19 +69,19 @@ const DEFAULT_DURATION = 220;
  *   const [visible, setVisible] = React.useState(false);
  *
  *   const showModal = () => setVisible(true);
- *
  *   const hideModal = () => setVisible(false);
+ *   const containerStyle = {backgroundColor: 'white', padding: 20};
  *
  *   return (
  *     <Provider>
  *       <Portal>
- *         <Modal visible={visible} onDismiss={hideModal}>
+ *         <Modal visible={visible} onDismiss={hideModal} contentContainerStyle={containerStyle}>
  *           <Text>Example Modal</Text>
  *         </Modal>
- *         <Button style={{marginTop: 30}} onPress={showModal}>
- *           Show
- *         </Button>
  *       </Portal>
+ *       <Button style={{marginTop: 30}} onPress={showModal}>
+ *         Show
+ *       </Button>
  *     </Provider>
  *   );
  * };
